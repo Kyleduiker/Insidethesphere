acb20ab379268e2352eb518fe739f7c589c53d7a
Kyleduiker <kyleduiker@royallepage.ca>
2025-08-08 09:44:47 -0600

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,17 +1,23 @@
-// Firebase Configuration for Inside the Sphere
-const firebaseConfig = {
-  apiKey: "AIzaSyAcHeHQdUmVwQ1s3RNGIJNNDjUGyAekgnM",
-  authDomain: "inside-the-sphere.firebaseapp.com",
-  projectId: "inside-the-sphere",
-  storageBucket: "inside-the-sphere.firebasestorage.app",
-  messagingSenderId: "318965201693",
-  appId: "1:318965201693:web:fa695da06154008e6cfff4",
-  measurementId: "G-H3Y4X2HEMB"
-};
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyAcHeHQdUmVwQls3RNGIJNNDjUGyAekgnM",
+    authDomain: "inside-the-sphere.firebaseapp.com",
+    projectId: "inside-the-sphere",
+    storageBucket: "inside-the-sphere.firebasestorage.app",
+    messagingSenderId: "318965201693",
+    appId: "1:318965201693:web:fa695da06154008e6cfff4",
+    measurementId: "G-H3Y4X2HEMB"
+  };
 
-// Create shortcuts for easy use
-const auth = firebase.auth();
-const db = firebase.firestore();
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
